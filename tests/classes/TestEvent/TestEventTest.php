656f37698d4b8c16fe9d19bf8625fef33113d334
shanliu <shanliu@msn.com>
2019-09-04 13:09:59 +0800

    ...

--- tests/classes/TestEvent/TestEventTest.php
+++ tests/classes/TestEvent/TestEventTest.php
@@ -8,16 +8,16 @@ final class EventTest extends TestCase
     public function testDispatch()
     {
         $data=null;
-        $call=new \LSYS\EventManager\EventCallback(function(TestEvent $event)use(&$data){
+        $call=new \LSYS\EventManager\EventCallback(TestEvent::name(TestEvent::TEST),function(TestEvent $event)use(&$data){
             $data=$event->data("sql");
         });
-        DI::get()->eventManager()->attach(TestEvent::name(TestEvent::TEST),$call);
+        DI::get()->eventManager()->attach($call);
         DI::get()->eventManager()->dispatch(new TestEvent(TestEvent::TEST,["sql"=>"1"]));
         $this->assertTrue($data=="1");
-        DI::get()->eventManager()->detach(TestEvent::name(TestEvent::TEST),$call);
+        DI::get()->eventManager()->detach($call);
         DI::get()->eventManager()->dispatch(new TestEvent(TestEvent::TEST,["sql"=>"2"]));
         $this->assertTrue($data=="1");
-        DI::get()->eventManager()->attach(TestEvent::name(TestEvent::TEST),$call,"test_handle");
+        DI::get()->eventManager()->attach($call,"test_handle");
         DI::get()->eventManager()->dispatch(new TestEvent(TestEvent::TEST,["sql"=>"3"]));
         $this->assertTrue($data=="3");
     }
