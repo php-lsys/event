8d2c6373c4f6822f70c6f2cd8d4df55e6b773c62
shanliu <shanliu@msn.com>
2019-09-06 13:58:24 +0800

    ..

--- tests/classes/TestEvent/TestEventTest.php
+++ tests/classes/TestEvent/TestEventTest.php
@@ -12,13 +12,13 @@ final class EventTest extends TestCase
             $data=$event->data("sql");
         });
         DI::get()->eventManager()->attach($call);
-        DI::get()->eventManager()->dispatch(new TestEvent(TestEvent::TEST,["sql"=>"1"]));
-        $this->assertTrue($data=="1");
+        DI::get()->eventManager()->dispatch(TestEvent::test(1));
+        $this->assertEquals($data,"1");
         DI::get()->eventManager()->detach($call);
-        DI::get()->eventManager()->dispatch(new TestEvent(TestEvent::TEST,["sql"=>"2"]));
-        $this->assertTrue($data=="1");
-        DI::get()->eventManager()->attach($call,"test_handle");
-        DI::get()->eventManager()->dispatch(new TestEvent(TestEvent::TEST,["sql"=>"3"]));
+        DI::get()->eventManager()->dispatch(TestEvent::test(3));
+        $this->assertEquals($data,"1");
+        DI::get()->eventManager()->attach($call);
+        DI::get()->eventManager()->dispatch(TestEvent::test(3));
         $this->assertTrue($data=="3");
     }
 }
