b6ae2a9ae2489779cc0cf7abad56a035528ef745
shanliu <shanliu@msn.com>
2020-08-05 16:33:45 +0800

    ..

--- tests/classes/TestEvent/TestEvent.php
+++ tests/classes/TestEvent/TestEvent.php
@@ -5,6 +5,6 @@ class TestEvent extends Event{
     //建议小写并用.号分割
     const TEST="test.dome";
     public static function test($sql) {
-        return new TestEvent(self::TEST,compact(func_get_argsname()));
+        return new static(static::TEST,func_get_args());
     }
 }
